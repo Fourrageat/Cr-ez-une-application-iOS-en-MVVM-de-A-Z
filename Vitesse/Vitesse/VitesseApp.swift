027da786e6818f3aa2482da170631004a9167ab7
Fourrageat <baptiste.fourrageat@icloud.com>
2025-12-02 20:52:59 +0100

    first commit iOS app

---
+++ Vitesse/Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Baptiste Fourrageat on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
